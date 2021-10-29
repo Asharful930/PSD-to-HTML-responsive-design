eff8044088a722ea4f121593d5dab399f5f4f694
Ashraful930 <ai676850@gmail.com>
2021-10-29 23:53:10 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function()
+  {
+    $('.slider_area').bxSlider();
+   });
+
+
+
+   jQuery(document).ready(function () {
+    jQuery('#main_menu').meanmenu({
+      meanMenuContainer:'#mobile_menu',
+      meanScreenWidth:991,
+    });
+  });
